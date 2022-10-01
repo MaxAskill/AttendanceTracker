9fae9072c3004f2b8400a4c986597a3293d56de0
Roland Alavera <roland@nbfi.local>
2022-10-01 14:20:38 +0800

    Adding feature in employee page

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -60,43 +60,117 @@
 					</tr>
 				</table>
 
+				<!-- <button class="border-xl bordercolor border-4 rounded-xl px-2 py-1 absolute inset-x-0 bottom-0 flex-shrink-0">Log Out</button> -->
 				<button class="border-xl bordercolor border-4 rounded-xl px-2 py-1">Log Out</button>
 			</div>
-      <!-- <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-gray-200 rounded-lg dark-mode:bg-gray-700 dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Blog</a>
-      <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Portfolio</a>
-      <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">About</a>
-      <a class="block px-4 py-2 mt-2 text-sm font-semibold text-gray-900 bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Contact</a>
-      <div @click.away="open = false" class="relative" x-data="{ open: false }">
-        <button @click="open = !open" class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm font-semibold text-left bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:focus:bg-gray-600 dark-mode:hover:bg-gray-600 md:block hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline">
-          <span>Dropdown</span>
-          <svg fill="currentColor" viewBox="0 0 20 20" :class="{'rotate-180': open, 'rotate-0': !open}" class="inline w-4 h-4 mt-1 ml-1 transition-transform duration-200 transform md:-mt-1"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-        </button>
-        <div x-show="open" x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95" x-transition:enter-end="transform opacity-100 scale-100" x-transition:leave="transition ease-in duration-75" x-transition:leave-start="transform opacity-100 scale-100" x-transition:leave-end="transform opacity-0 scale-95" class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg">
-          <div class="px-2 py-2 bg-white rounded-md shadow dark-mode:bg-gray-800">
-            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #1</a>
-            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #2</a>
-            <a class="block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark-mode:bg-transparent dark-mode:hover:bg-gray-600 dark-mode:focus:bg-gray-600 dark-mode:focus:text-white dark-mode:hover:text-white dark-mode:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline" href="#">Link #3</a>
-          </div>
-        </div>
-      </div> -->
     </nav>
   </sidebar>
 </div>
 
 <content class="xl:w-9/12 sm:w-full px-20">
         <div class="grid md:grid-flow-row grid-rows-2 grid-cols-4">
-			<div class="row-start-1 row-span-1 bg-green-900 px-5 py-5">
-				<div class="bg-gray-700 rounded-lg">
+			<div class="row-start-1 row-span-1 bg-green-900 pt-2">
+				<div class="bg-gray-500 rounded-lg px-2 py-2 text-white text-center">
 					<h1>Worked Hour Today</h1>
 					<span class="countdown font-mono text-2xl">
-						<span style="--value:10;"></span>h
-						<span style="--value:24;"></span>m
-						<span style="--value:4;"></span>s
+						07:30:20
 					</span>
 				</div>
 			</div>
-			<div class="row-start-2 row-span-1 bg-red-900">02</div>
-			<div class="row-start-1 row-span-2 col-span-3 bg-blue-900">09</div>
+			<div class="row-start-2 row-span-1 bg-red-900 pt-2">
+				<div class="bg-gray-500 rounded-lg px2 py-2 text-center text-white">
+					<h1>Timed In Today</h1>
+					<span class="countdown font-mono text-2xl text-center ">
+						<span style="--value:10;"></span>07
+						<span style="--value:24;"></span>30
+						<span style="--value:4;"></span>00
+					</span>
+				</div>
+			</div>
+			<div class="row-start-1 row-span-2 col-span-3 bg-blue-900 pt-2 pl-5">
+				<div class="flex h-full bg-white rounded-lg px-2 py-2 grid md:grid-flow-row grid-rows-2 grid-cols-4">
+					<!-- <span class="countdown font-mono text-2xl text-center ">
+						<span style="--value:10;"></span>07
+						<span style="--value:24;"></span>30
+						<span style="--value:4;"></span>00
+					</span> -->
+					<div class="row-start-1  col-span-1 bg-green-900">
+						<h1>Summary</h1>
+						<h2>SEPTEMBER 2022</h2>
+					</div>
+					<div class="row-start-1  col-span-3 bg-red-900 flex items-center ">
+						<button class="border-2 border-solid bordercolor rounded-2xl px-2 py-2 font-themecolor">View More</button>
+					</div>
+					<div class="bg-blue-900 text-center">
+						<h1>Late</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-brown-900 text-center">
+						<h1>Undertime</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-orange-900 text-center">
+						<h1>Overtime</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-gray-900 text-center">
+						<h1>Hours Rendered</h1>
+						<h2>0 Day/s</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="grid md:grid-flow-row grid-rows-2 grid-cols-4">
+			<div class="row-start-1 row-span-1 bg-green-900 pt-2">
+				<div class="bg-gray-500 rounded-lg px-2 py-2 text-white text-center">
+					<h1>Worked Hour Today</h1>
+					<span class="countdown font-mono text-2xl">
+						07:30:20
+					</span>
+				</div>
+			</div>
+			<div class="row-start-2 row-span-1 bg-red-900 pt-2">
+				<div class="bg-gray-500 rounded-lg px2 py-2 text-center text-white">
+					<h1>Timed In Today</h1>
+					<span class="countdown font-mono text-2xl text-center ">
+						<span style="--value:10;"></span>07
+						<span style="--value:24;"></span>30
+						<span style="--value:4;"></span>00
+					</span>
+				</div>
+			</div>
+			<div class="row-start-1 row-span-2 col-span-3 bg-blue-900 pt-2 pl-5">
+				<div class="flex h-full bg-white rounded-lg px-2 py-2 grid md:grid-flow-row grid-rows-2 grid-cols-4">
+					<!-- <span class="countdown font-mono text-2xl text-center ">
+						<span style="--value:10;"></span>07
+						<span style="--value:24;"></span>30
+						<span style="--value:4;"></span>00
+					</span> -->
+					<div class="row-start-1  col-span-1 bg-green-900">
+						<h1>Summary</h1>
+						<h2>SEPTEMBER 2022</h2>
+					</div>
+					<div class="row-start-1  col-span-3 bg-red-900 flex items-center ">
+						<button class="border-2 border-solid bordercolor rounded-2xl px-2 py-2 font-themecolor">View More</button>
+					</div>
+					<div class="bg-blue-900 text-center">
+						<h1>Late</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-brown-900 text-center">
+						<h1>Undertime</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-orange-900 text-center">
+						<h1>Overtime</h1>
+						<h2>0 Day/s</h2>
+					</div>
+					<div class="bg-gray-900 text-center">
+						<h1>Hours Rendered</h1>
+						<h2>0 Day/s</h2>
+					</div>
+				</div>
+			</div>
 		</div>
 </content>
 
