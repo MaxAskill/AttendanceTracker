c27334a54e8b76855e8cb58e48ff6a94f3682157
Roland Alavera <roland@nbfi.local>
2022-10-06 13:52:13 +0800

    changing colors and text weight in denied modal in both employee and approver page

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -746,16 +746,16 @@
 
   <!-- Main modal -->
 <div id="denied-ot" aria-hidden="true" class="hidden overflow-x-hidden overflow-y-auto fixed h-modal md:h-full top-4 left-0 right-0 md:inset-0 z-50 justify-center items-center">
-        <div class="relative xl:w-2/3 sm:w-full xl:px-4 h-full md:h-auto">
+        <div class="relative xl:w-2/5 sm:w-full xl:px-4 h-full md:h-auto">
             <!-- Modal content -->
-            <div class="bg-white rounded-lg shadow relative dark:bg-gray-700">
+            <div class="bg-white rounded-lg shadow relative dark:bg-gray-700 border-4 border-solid border-pantone">
                 <div class="flex justify-end p-2">
                     <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="denied-ot">
                         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>  
                     </button>
                 </div>
                 <form class="space-y-3 px-6 lg:px-8 pb-4 sm:pb-6 xl:pb-8" action="#">
-                    <h3 class="text-xl font-medium text-gray-900 dark:text-white">OVERTIME REQUEST DENIED!</h3>
+                    <h3 class="text-xl font-medium text-center font-pantone text-gray-900 dark:text-white font-extrabold">OVERTIME REQUEST DENIED!</h3>
                    
 					<div>
                         <label for="text" class="capitalized text-sm font-medium text-gray-900 block mb-2 dark:text-gray-300">Applicant Name: Maria M. Clara</label>
@@ -772,9 +772,9 @@
 					<div>
 					<div class="flex justify-center">
 						<div class="mb-3 xl:w-full">
-							<label for="exampleFormControlTextarea1" class="form-label inline-block mb-2 text-gray-700">Reason</label>
+							<label for="exampleFormControlTextarea1" class="form-label inline-block mb-2 text-gray-700 font-bold">Reason</label>
 									<textarea
-							class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+							class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border-2 border-solid border-pantone rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
 							id="exampleFormControlTextarea1"
 							rows="8"
 							placeholder="Your messge" disabled>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi in finibus enim. Phasellus nec dolor scelerisque, scelerisque sapien quis, viverra elit. Vestibulum vel aliquet nibh. Vivamus quis blandit est. Nullam id posuere velit, sed molestie justo. 
@@ -1008,7 +1008,7 @@
 					<div>
 					<div class="flex justify-center">
 						<div class="mb-3 xl:w-full">
-							<label for="exampleFormControlTextarea1" class="form-label inline-block mb-2 text-gray-700">Reason</label>
+							<label for="exampleFormControlTextarea1" class="form-label inline-block mb-2 text-gray-700 font-bold">Reason</label>
 									<textarea
 							class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
 							id="exampleFormControlTextarea1"
