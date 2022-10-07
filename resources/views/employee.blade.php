9c0ecd9d868f520138adb04942943aedce54f402
Melvin Perez <melvin@nbfi.local>
2022-10-07 18:07:03 +0800

    upload fixed for images only in employee

--- resources/views/employee.blade.php
+++ resources/views/employee.blade.php
@@ -1231,7 +1231,7 @@
                                         </svg>
                                         <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span
                                                 class="font-semibold">Click to upload</span> or drag and drop</p>
-                                        <p class="text-xs text-gray-500 dark:text-gray-400">(XLS, XLSX)</p>
+                                        <p class="text-xs text-gray-500 dark:text-gray-400">(PNG, IMG, JPEG, JPG, PDF)</p>
                                     </div>
                                     <input id="dropzone-file" type="file" class="hidden">
                                 </label>
