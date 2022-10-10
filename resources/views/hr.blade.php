73ebc65cc563078ce62a944f4736c566a6ae8848
Melvin Perez <melvin@nbfi.local>
2022-10-10 14:31:45 +0800

    adjust to red color button on resign modal

--- resources/views/hr.blade.php
+++ resources/views/hr.blade.php
@@ -2763,7 +2763,7 @@
                                 Are you really sure?
                                 </h3>
                             <button data-modal-toggle="resign-modal" type="button"
-                                class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                                class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
                                 Yes, I am sure
                             </button>
                             <button data-modal-toggle="resign-modal" type="button"
